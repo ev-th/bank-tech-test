6188c1ccb03d38daabf54445113787b32acab70b
Evan Thomas <evanthomas0292@gmail.com>
2023-06-18 13:27:09 +0100

    Write functionality for making a deposit and it shows up on the statement

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,39 @@
 class BankAccount
+  def initialize
+    @balance = 0
+    @transfers = []
+  end
+
   def statement
-    return "date || credit || debit || balance"
+    header = "date || credit || debit || balance" 
+    return header + "\n" + deposit_to_s(@transfers[0]) if @transfers.length == 1
+    return header
+  end
+
+  def deposit(amount, current_time)
+    @balance += amount
+
+    transfer = {
+      date: current_time,
+      credit: amount,
+      debit: 0,
+      balance: @balance
+    }
+
+    @transfers.push(transfer)
+  end
+
+  private
+
+  def deposit_to_s(transfer)
+    # binding.irb
+    return transfer[:date].strftime("%d/%m/%Y") +
+    " || " + transfer_amount_to_s(transfer[:credit]) +
+    " || " + 
+    "|| " + transfer_amount_to_s(transfer[:balance])
+  end
+
+  def transfer_amount_to_s(amount)
+    sprintf("%.2f", amount.to_f)
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe BankAccount do
       expect(result).to eq "date || credit || debit || balance"
     end
   end
+  
+  context "when the client makes a deposit" do
+    it "shows up on her bank statement with a total" do
+      bank_account = BankAccount.new
+      time = Time.new(2023, 01, 10)
+      bank_account.deposit(1000, time)
+      result = bank_account.statement
+      expect(result).to eq "date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00"
+    end
+  end
 end
