1bd8c437ea8ef8ab3aafae26402dbedf4b0a441d
Evan Thomas <evanthomas0292@gmail.com>
2023-06-18 15:39:10 +0100

    Add rubocop and fix most of the warnings

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,25 +5,25 @@ class BankAccount
   end
 
   def statement
-    rows = ["date || credit || debit || balance"]
-    rows += @transfers.reverse.map { |transfer| transfer_to_s(transfer)}
+    rows = ['date || credit || debit || balance']
+    rows += @transfers.reverse.map { |transfer| transfer_to_s(transfer) }
 
-    return rows.join("\n")
+    rows.join('\n')
   end
 
-  def deposit(amount, current_date=Time.now)
-    return make_transfer(current_date, credit: amount)
+  def deposit(amount, current_date = Time.now)
+    make_transfer(current_date, credit: amount)
   end
-  
-  def withdraw(amount, current_date=Time.now)
-    return make_transfer(current_date, debit: amount)
+
+  def withdraw(amount, current_date = Time.now)
+    make_transfer(current_date, debit: amount)
   end
 
   private
-  
+
   def make_transfer(current_date, credit: 0, debit: 0)
     @balance += credit - debit
-    
+
     transfer = {
       date: current_date,
       credit: credit,
@@ -33,24 +33,24 @@ class BankAccount
 
     @transfers.push(transfer)
   end
-  
+
   def transfer_to_s(transfer)
     transfer_s = "#{format_date(transfer[:date])} || "
-    
+
     if transfer[:debit].zero?
       transfer_s += "#{format_money(transfer[:credit])} ||"
     elsif transfer[:credit].zero?
       transfer_s += "|| #{format_money(transfer[:debit])}"
     end
 
-    return transfer_s + " || #{format_money(transfer[:balance])}"
+    transfer_s + " || #{format_money(transfer[:balance])}"
   end
 
   def format_money(amount)
-    return sprintf("%.2f", amount.to_f)
+    format('%.2f', amount.to_f)
   end
 
   def format_date(date)
-    return date.strftime("%d/%m/%Y")
+    date.strftime('%d/%m/%Y')
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,61 +1,61 @@
 require 'bank_account'
 
 RSpec.describe BankAccount do
-  context "when the client sets up an account" do
-    it "displays an empty bank statement" do
+  context 'when the client sets up an account' do
+    it 'displays an empty bank statement' do
       bank_account = BankAccount.new
 
       result = bank_account.statement
-      expected_result = "date || credit || debit || balance"
-      
+      expected_result = 'date || credit || debit || balance'
+
       expect(result).to eq expected_result
     end
   end
-  
-  context "when the client makes a deposit" do
-    it "shows up on her bank statement with a total" do
+
+  context 'when the client makes a deposit' do
+    it 'shows up on her bank statement with a total' do
       bank_account = BankAccount.new
-      date = Time.new(2023, 01, 10)
+      date = Time.new(2023, 1, 10)
       bank_account.deposit(1000, date)
-      
+
       result = bank_account.statement
-      expected_result = "date || credit || debit || balance\n" +
-        "10/01/2023 || 1000.00 || || 1000.00"
-      
-      expect(result).to eq expected_result
-    end
-  end
-  
-  context "when the client makes a withdrawal" do
-    it "shows up on her bank statement with a total" do
-      bank_account = BankAccount.new
-      date = Time.new(2023, 01, 14)
-      bank_account.withdraw(500, date)
-      
-      result = bank_account.statement
-      expected_result = "date || credit || debit || balance\n" +
-        "14/01/2023 || || 500.00 || -500.00"
+      expected_result = 'date || credit || debit || balance\n' \
+        '10/01/2023 || 1000.00 || || 1000.00'
 
       expect(result).to eq expected_result
     end
   end
-  
-  context "when a client makes multiple transfers" do
-    it "shows all the transfers on the bank statement" do
+
+  context 'when the client makes a withdrawal' do
+    it 'shows up on her bank statement with a total' do
       bank_account = BankAccount.new
-      date1 = Time.new(2023, 01, 10)
-      bank_account.deposit(1000, date1)
-      date2 = Time.new(2023, 01, 13)
-      bank_account.deposit(2000, date2)
-      date3 = Time.new(2023, 01, 14)
-      bank_account.withdraw(500, date3)
-      
+      date = Time.new(2023, 1, 14)
+      bank_account.withdraw(500, date)
+
       result = bank_account.statement
-      expected_result = "date || credit || debit || balance\n" +
-        "14/01/2023 || || 500.00 || 2500.00\n" +
-        "13/01/2023 || 2000.00 || || 3000.00\n" +
-        "10/01/2023 || 1000.00 || || 1000.00"
-    
+      expected_result = 'date || credit || debit || balance\n' \
+        '14/01/2023 || || 500.00 || -500.00'
+
+      expect(result).to eq expected_result
+    end
+  end
+
+  context 'when a client makes multiple transfers' do
+    it 'shows all the transfers on the bank statement' do
+      bank_account = BankAccount.new
+      date1 = Time.new(2023, 1, 10)
+      bank_account.deposit(1000, date1)
+      date2 = Time.new(2023, 1, 13)
+      bank_account.deposit(2000, date2)
+      date3 = Time.new(2023, 1, 14)
+      bank_account.withdraw(500, date3)
+
+      result = bank_account.statement
+      expected_result = 'date || credit || debit || balance\n' \
+        '14/01/2023 || || 500.00 || 2500.00\n' \
+        '13/01/2023 || 2000.00 || || 3000.00\n' \
+        '10/01/2023 || 1000.00 || || 1000.00'
+
       expect(result).to eq expected_result
     end
   end
