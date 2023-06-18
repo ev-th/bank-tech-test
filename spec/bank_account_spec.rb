56fe446fbc89013419b0a36a711c6648344ce5d7
Evan Thomas <evanthomas0292@gmail.com>
2023-06-18 14:04:13 +0100

    Refactor tests for readability

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe BankAccount do
   context "when the client sets up an account" do
     it "displays an empty bank statement" do
       bank_account = BankAccount.new
+
       result = bank_account.statement
-      expect(result).to eq "date || credit || debit || balance"
+      expected_result = "date || credit || debit || balance"
+      
+      expect(result).to eq expected_result
     end
   end
   
@@ -14,8 +17,12 @@ RSpec.describe BankAccount do
       bank_account = BankAccount.new
       date = Time.new(2023, 01, 10)
       bank_account.deposit(1000, date)
+      
       result = bank_account.statement
-      expect(result).to eq "date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00"
+      expected_result = "date || credit || debit || balance\n" +
+        "10/01/2023 || 1000.00 || || 1000.00"
+      
+      expect(result).to eq expected_result
     end
   end
   
@@ -24,28 +31,32 @@ RSpec.describe BankAccount do
       bank_account = BankAccount.new
       date = Time.new(2023, 01, 14)
       bank_account.withdrawal(500, date)
+      
       result = bank_account.statement
-      expect(result).to eq "date || credit || debit || balance\n14/01/2023 || || 500.00 || -500.00"
+      expected_result = "date || credit || debit || balance\n" +
+        "14/01/2023 || || 500.00 || -500.00"
+
+      expect(result).to eq expected_result
     end
   end
   
   context "when a client makes multiple transfers" do
     it "shows all the transfers on the bank statement" do
       bank_account = BankAccount.new
-      
       date1 = Time.new(2023, 01, 10)
       bank_account.deposit(1000, date1)
-      
       date2 = Time.new(2023, 01, 13)
       bank_account.deposit(2000, date2)
-      
       date3 = Time.new(2023, 01, 14)
       bank_account.withdrawal(500, date3)
       
       result = bank_account.statement
-      # binding.irb
-
-      expect(result).to eq "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+      expected_result = "date || credit || debit || balance\n" +
+        "14/01/2023 || || 500.00 || 2500.00\n" +
+        "13/01/2023 || 2000.00 || || 3000.00\n" +
+        "10/01/2023 || 1000.00 || || 1000.00"
+    
+      expect(result).to eq expected_result
     end
   end
 end
