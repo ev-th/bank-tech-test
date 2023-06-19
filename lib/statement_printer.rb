2af61b62c2d7a4ca5b793f5a11ad0c1ea4207315
Evan Thomas <evanthomas0292@gmail.com>
2023-06-19 21:38:10 +0100

    Write unit test for printing a statement with a deposit. Extract the functionality for printing from BankAccount into StatementPrinter. Printer calculates running total from transfer amounts while generating

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,5 +1,36 @@
 class StatementPrinter
   def generate_statement(account)
-    return 'date || credit || debit || balance'
+    rows = ['date || credit || debit || balance']
+    balance = account.starting_balance
+
+    rows += account.transfers.reverse.map do |transfer| 
+      balance += transfer[:credit] - transfer[:debit]
+      transfer_to_s(transfer, balance)
+    end
+
+    rows.join("\n")
+  end
+
+  private
+  
+  def transfer_to_s(transfer, balance)
+    
+    transfer_s = "#{format_date(transfer[:date])} || "
+
+    if transfer[:debit].zero?
+      transfer_s += "#{format_money(transfer[:credit])} ||"
+    elsif transfer[:credit].zero?
+      transfer_s += "|| #{format_money(transfer[:debit])}"
+    end
+
+    transfer_s + " || #{format_money(balance)}"
+  end
+
+  def format_money(amount)
+    format('%.2f', amount.to_f)
+  end
+
+  def format_date(date)
+    date.strftime('%d/%m/%Y')
   end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -3,14 +3,32 @@ require 'statement_printer'
 RSpec.describe StatementPrinter do
   context "when the account has no transfers" do
     it 'generates an empty bank statement' do
-      fake_bank_account = double :fake_bank_account
+      fake_bank_account = double :fake_bank_account, starting_balance: 0
       allow(fake_bank_account).to receive(:transfers).and_return([])
-
       printer = StatementPrinter.new
-
 
       statement = printer.generate_statement(fake_bank_account)
       expected_result = 'date || credit || debit || balance'
+
+      expect(statement).to eq expected_result
+    end
+  end
+
+  context "when the account has a deposit" do
+    it 'generates an bank statement with the deposit and current total' do
+      fake_bank_account = double :fake_bank_account, starting_balance: 0
+      allow(fake_bank_account).to receive(:transfers).and_return([
+        {
+          date: Time.new(2023, 1, 10),
+          credit: 1000,
+          debit: 0
+        }
+      ])
+      printer = StatementPrinter.new
+
+      statement = printer.generate_statement(fake_bank_account)
+      expected_result = "date || credit || debit || balance\n" \
+        '10/01/2023 || 1000.00 || || 1000.00'
 
       expect(statement).to eq expected_result
     end
