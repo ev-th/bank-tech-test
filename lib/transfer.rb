659671b8a24bb0880e1266fd09200e7bcee4a242
Evan Thomas <evanthomas0292@gmail.com>
2023-06-20 12:01:16 +0100

    Transfer can check if it's a deposit or withdrawal. Transfer cannot be initialized with 0

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,17 @@
 class Transfer
   attr_reader :amount
   def initialize(amount)
+    fail 'The transfer amount cannot be 0' if amount.zero?
+    
+    @type = amount.positive? ? 'deposit' : 'withdrawal'
     @amount = amount.abs
+  end
+
+  def deposit?
+    @type == 'deposit'
+  end
+
+  def withdrawal?
+    @type == 'withdrawal'
   end
 end
--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -1,13 +1,29 @@
 require 'transfer'
 
 RSpec.describe Transfer do
-  it "stores the money transferred by a deposit" do
+  it 'stores the money transferred by a deposit' do
     transfer = Transfer.new(100)
     expect(transfer.amount).to eq 100
   end
 
-  it "stores the money transferred by a withdrawal" do
+  it 'stores the money transferred by a withdrawal' do
     transfer = Transfer.new(-2000)
     expect(transfer.amount).to eq 2000
+  end
+  
+  it 'is a deposit if passed a positive number' do
+    transfer = Transfer.new(700)
+    expect(transfer.deposit?).to eq true
+    expect(transfer.withdrawal?).to eq false
+  end
+  
+  it 'is a withdrawal if passed a negative number' do
+    transfer = Transfer.new(-3)
+    expect(transfer.deposit?).to eq false
+    expect(transfer.withdrawal?).to eq true
+  end
+  
+  it 'fails to initialize with 0' do
+    expect { Transfer.new(0) }.to raise_error 'The transfer amount cannot be 0'
   end
 end
