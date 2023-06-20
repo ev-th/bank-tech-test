c38cdc93be7529acd418955e2881350fea613a42
Evan Thomas <evanthomas0292@gmail.com>
2023-06-20 17:04:28 +0100

    Begin refactoring formatting logic to make method responsibilities more clear

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -27,23 +27,39 @@ class StatementPrinter
   end
 
   def format_transfer(transfer, updated_balance)
-    columns = ["#{format_date(transfer.timestamp)} || "]
+    credit = transfer.deposit? ? transfer.amount : 0
+    debit = transfer.withdrawal? ? transfer.amount : 0
 
-    if transfer.deposit?
-      columns.push "#{format_money(transfer.amount)} ||"
-    elsif transfer.withdrawal?
-      columns.push "|| #{format_money(transfer.amount)}"
-    end
+    row = [
+      format_date(transfer.timestamp),
+      credit.zero? ? "" : format_money(credit),
+      debit.zero? ? "" : format_money(debit),
+      format_money(updated_balance)
+    ]
 
-    columns.push " || #{format_money(updated_balance)}"
-    columns.join
+    join_row(row)
   end
 
-  def format_money(amount)
-    format('%.2f', amount.to_f)
+  def join_row(row)
+    formatted_elements = row.map.with_index do |element, i|
+      if i.zero?
+        "#{element} "
+      elsif i == row.length - 1
+        " #{element}"
+      elsif element.empty?
+        " "
+      else
+        " #{element} "
+      end
+    end
+    formatted_elements.join("||")
   end
 
   def format_date(date)
-    date.strftime('%d/%m/%Y')
+    "#{date.strftime('%d/%m/%Y')}"
+  end
+
+  def format_money(amount)
+    "#{format('%.2f', amount.to_f)}"
   end
 end
